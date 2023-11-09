79a963c13159dab0aeee1d847c7e4300e235dc5c
abhinayy58 <abhinayy48@gmail.com>
2023-11-09 12:08:46 +0530

    latest

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const path = require("path")
 const bodyParser = require("body-parser");
 const authRoute = require("./routes/Auth.js");
 const userRoute = require("./routes/User.js");
@@ -34,11 +35,20 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 // app.use(cookieParser());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/rooms", roomRoute);
 app.use("/api/booking", bookingRoute);
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
